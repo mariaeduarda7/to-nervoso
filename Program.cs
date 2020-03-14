0ebeb6c5ddc309009747ef2b2991ab2227f33af1
maria eduarda <duda71771@gmail.com>
2020-03-13 22:40:25 -0300

    exercicio

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace to_nervoso
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Usuário, o que você pensa sobre mim?");
+            string mensagem = Console.ReadLine();
+            string tonervoso = mensagem.Remove(10) + "... nao pera";
+            Console.WriteLine(tonervoso);
+            Console.WriteLine("to nervoso");
+        }   
+    }
+}
